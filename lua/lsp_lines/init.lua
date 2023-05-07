f13cc1338e5ce1e7b61e3833a4d9a4436646cf9c
Vladimir Zykov <vova@home>
2023-05-07 14:18:00 +0300

    fix stuff

--- lua/lsp_lines/init.lua
+++ lua/lsp_lines/init.lua
@@ -1,138 +1,58 @@
 local M = {}
 
-local highlight_groups = {
-  [vim.diagnostic.severity.ERROR] = "DiagnosticVirtualTextError",
-  [vim.diagnostic.severity.WARN] = "DiagnosticVirtualTextWarn",
-  [vim.diagnostic.severity.INFO] = "DiagnosticVirtualTextInfo",
-  [vim.diagnostic.severity.HINT] = "DiagnosticVirtualTextHint",
-}
+local render = require("lsp_lines.render")
 
-local SPACE = 0
-local DIAGNOSTIC = 1
-local OVERLAP = 2
+local function render_current_line(diagnostics, ns, bufnr, opts)
+  local current_line_diag = {}
+  local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
+
+  for _, diagnostic in pairs(diagnostics) do
+    local show = diagnostic.end_lnum and (lnum >= diagnostic.lnum and lnum <= diagnostic.end_lnum)
+      or (lnum == diagnostic.lnum)
+    if show then
+      table.insert(current_line_diag, diagnostic)
+    end
+  end
+
+  render.show(ns, bufnr, current_line_diag, opts)
+end
+
+---@class Opts
+---@field virtual_lines OptsVirtualLines Options for lsp_lines plugin
+
+---@class OptsVirtualLines
+---@field only_current_line boolean Only render for current line
+---@field highlight_whole_line boolean Highlight empty space to the left of a diagnostic
 
 -- Registers a wrapper-handler to render lsp lines.
 -- This should usually only be called once, during initialisation.
-M.register_lsp_virtual_lines = function()
-  -- TODO: When a diagnostic changes for the current line, the cursor is not shifted properly.
+M.setup = function()
+  vim.api.nvim_create_augroup("LspLines", { clear = true })
   -- TODO: On LSP restart (e.g.: diagnostics cleared), errors don't go away.
-
   vim.diagnostic.handlers.virtual_lines = {
     ---@param namespace number
     ---@param bufnr number
     ---@param diagnostics table
-    ---@param opts boolean
+    ---@param opts boolean|Opts
     show = function(namespace, bufnr, diagnostics, opts)
-      vim.validate({
-        namespace = { namespace, "n" },
-        bufnr = { bufnr, "n" },
-        diagnostics = {
-          diagnostics,
-          vim.tbl_islist,
-          "a list of diagnostics",
-        },
-        opts = { opts, "t", true },
-      })
-
-      table.sort(diagnostics, function(a, b)
-        if a.lnum ~= b.lnum then
-          return a.lnum < b.lnum
-        else
-          return a.col < b.col
-        end
-      end)
-
       local ns = vim.diagnostic.get_namespace(namespace)
       if not ns.user_data.virt_lines_ns then
         ns.user_data.virt_lines_ns = vim.api.nvim_create_namespace("")
       end
-      local virt_lines_ns = ns.user_data.virt_lines_ns
 
-      vim.api.nvim_buf_clear_namespace(bufnr, virt_lines_ns, 0, -1)
-
-      local line_stacks = {}
-      local prev_lnum = -1
-      local prev_col = -1
-      for id, diagnostic in ipairs(diagnostics) do
-        diagnostic.id = id -- Tie the id to the actual object.
-
-        if line_stacks[diagnostic.lnum] == nil then
-          line_stacks[diagnostic.lnum] = {}
-        end
-
-        local stack = line_stacks[diagnostic.lnum]
-
-        if diagnostic.lnum ~= prev_lnum then
-          table.insert(stack, { SPACE, string.rep(" ", diagnostic.col) })
-          table.insert(stack, { DIAGNOSTIC, diagnostic })
-        elseif diagnostic.col ~= prev_col then
-          table.insert(stack, { SPACE, string.rep(" ", diagnostic.col - prev_col - 1) })
-          table.insert(stack, { DIAGNOSTIC, diagnostic })
-        else
-          table.insert(stack, { OVERLAP, diagnostic.severity })
-          table.insert(stack, { DIAGNOSTIC, diagnostic })
-        end
-
-        prev_lnum = diagnostic.lnum
-        prev_col = diagnostic.col
-      end
-
-      for lnum, lelements in pairs(line_stacks) do
-        local virt_lines = {}
-        for i = #lelements, 1, -1 do -- last element goes on top
-          if lelements[i][1] == DIAGNOSTIC then
-            local diagnostic = lelements[i][2]
-
-            local left = {}
-            local overlap = false
-
-            -- Iterate the stack for this line to find elements on the left.
-            for j = 1, i - 1 do
-              local type = lelements[j][1]
-              local data = lelements[j][2]
-              if type == SPACE then
-                table.insert(left, { data, "" })
-              elseif type == DIAGNOSTIC then
-                -- If an overlap follows this, don't add an extra column.
-                if lelements[j + 1][1] ~= OVERLAP then
-                  table.insert(left, { "│", highlight_groups[data.severity] })
-                end
-                overlap = false
-              elseif type == OVERLAP then
-                overlap = true
-              end
-            end
-
-            local center
-            if overlap then
-              center = { { "├──── ", highlight_groups[diagnostic.severity] } }
-            else
-              center = { { "└──── ", highlight_groups[diagnostic.severity] } }
-            end
-
-            for msg_line in diagnostic.message:gmatch("([^\n]+)") do
-              local vline = {}
-              vim.list_extend(vline, left)
-              vim.list_extend(vline, center)
-              vim.list_extend(vline, { { msg_line, highlight_groups[diagnostic.severity] } })
-
-              table.insert(virt_lines, vline)
-
-              -- Special-case for continuation lines:
-              if overlap then
-                center = { { "│", highlight_groups[diagnostic.severity] }, { "     ", "" } }
-              else
-                center = { { "      ", "" } }
-              end
-            end
-          end
-        end
-
-        vim.api.nvim_buf_set_extmark(bufnr, virt_lines_ns, lnum, 0, {
-          id = lnum,
-          virt_lines = virt_lines,
-          virt_lines_above = false,
+      vim.api.nvim_clear_autocmds({ group = "LspLines" })
+      if opts.virtual_lines.only_current_line then
+        vim.api.nvim_create_autocmd("CursorMoved", {
+          buffer = bufnr,
+          callback = function()
+            render_current_line(diagnostics, ns.user_data.virt_lines_ns, bufnr, opts)
+          end,
+          group = "LspLines",
         })
+        -- Also show diagnostics for the current line before the first CursorMoved event
+        render_current_line(diagnostics, ns.user_data.virt_lines_ns, bufnr, opts)
+      else
+        render.show(ns.user_data.virt_lines_ns, bufnr, diagnostics, opts)
       end
     end,
     ---@param namespace number
@@ -140,10 +60,18 @@ M.register_lsp_virtual_lines = function()
     hide = function(namespace, bufnr)
       local ns = vim.diagnostic.get_namespace(namespace)
       if ns.user_data.virt_lines_ns then
-        vim.api.nvim_buf_clear_namespace(bufnr, ns.user_data.virt_lines_ns, 0, -1)
+        render.hide(ns.user_data.virt_lines_ns, bufnr)
+        vim.api.nvim_clear_autocmds({ group = "LspLines" })
       end
     end,
   }
+end
+
+---@return boolean
+M.toggle = function()
+  local new_value = not vim.diagnostic.config().virtual_lines
+  vim.diagnostic.config({ virtual_lines = new_value })
+  return new_value
 end
 
 return M
